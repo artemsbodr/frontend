ee320726c84a71ddc0ed595b87b46c12b8c1edc2
MrArtem <artembodrik210311@gmail.com>
2025-09-27 14:40:50 +0300

    push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Гриша</h1>
+      <p>Ласкаво просимо до нашого сайту!</p>
+    </>
+  )
+}
+
+export default App
